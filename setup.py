1e655e901652e9e6176dcd3b606df2ebb4ea880a
Emanuele Fabbiani <donlelef@gmail.com>
2018-09-08 15:43:19 +0200

    Change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vanilla_option_pricing',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     url='',
     license='MIT',
