eecb8ab4987af6dca804e61b1ed9cca1f354a338
Emanuele Fabbiani <donlelef@gmail.com>
2020-10-10 13:40:33 +0200

    test: add test for consistency between numerical and analytical variance

--- tests/test_lmrgw_model.py
+++ tests/test_lmrgw_model.py
@@ -1,3 +1,5 @@
+import itertools
+
 import numpy as np
 import pytest
 from pytest import fixture
@@ -32,3 +34,17 @@ def test_numerical_variance(numerical_model):
 
 def test_numerical_properties(model):
     assert model.parameters == (1, 1, 0.05)
+
+
+def test_analytical_numerical_consistency():
+    p_0 = np.eye(2)
+    ls = s_xs = s_ys = list(np.linspace(0.5, 5, 10))
+    ts = np.linspace(10.0 / 365.0, 2, 10)
+    params = list(itertools.product(ls, s_xs, s_ys))
+    for l, s_x, s_y in params:
+        for t in ts:
+            lmrgw = LogMeanRevertingToGeneralisedWienerProcess(p_0, l, s_x, s_y)
+            num_lmrgw = NumericalLogMeanRevertingToGeneralisedWienerProcess(p_0, l, s_x, s_y)
+            var = lmrgw.variance(t)
+            num_var = num_lmrgw.variance(t)
+            assert var == pytest.approx(num_var, 10e-8)
